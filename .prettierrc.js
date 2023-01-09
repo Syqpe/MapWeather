fc141259e113ed9e04148d8d30cd66aa88a55eda
Abat <kasabat001@gmail.com>
2023-01-10 01:30:28 +0600

    feat: init project

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    tabWidth: 4,
+    singleQuote: false,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    trailingComma: 'all',
+};
---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./src";
+import { name as appName } from "./src/index.json";
+
+AppRegistry.registerComponent(appName, () => App);
