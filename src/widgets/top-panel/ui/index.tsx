ae864e69ae5a91df90dd4d01f121c5344216ccf4
Abat <kasabat001@gmail.com>
2023-02-20 01:44:34 +0600

    feat: added widget TopPanel

---
+++ src/widgets/top-panel/ui/index.tsx
@@ -0,0 +1,72 @@
+import React, { FC } from "react";
+import { View, TouchableOpacity } from "react-native";
+import { makeStyles } from "@rneui/themed";
+
+import { Icon } from "@components/index";
+
+type ISide = "left" | "right";
+
+interface Props {
+    iconName?: string;
+    routeName: string;
+    side?: ISide;
+    isBackNavigation?: boolean;
+    navigation?: any;
+}
+
+const TopPanel: FC<Props> = function ({
+    iconName,
+    routeName,
+    side = "right",
+    isBackNavigation,
+    navigation,
+}) {
+    const styles = useStyles();
+
+    const handlePress = () => {
+        if (isBackNavigation) {
+            navigation?.goBack?.();
+        } else {
+            navigation?.navigate?.(routeName);
+        }
+    };
+
+    return (
+        <View
+            style={[
+                styles.container,
+                {
+                    justifyContent:
+                        side === "left" || isBackNavigation
+                            ? "flex-start"
+                            : "flex-end",
+                },
+            ]}
+        >
+            <TouchableOpacity onPress={handlePress}>
+                <Icon
+                    name={
+                        isBackNavigation
+                            ? "arrow-back"
+                            : iconName
+                            ? iconName
+                            : "sync"
+                    }
+                    type="ionicon"
+                    size={22}
+                />
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const useStyles = makeStyles(() => ({
+    container: {
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        marginBottom: 10,
+    },
+}));
+
+export { TopPanel };
