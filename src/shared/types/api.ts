b0a56c05fccf4b0c4ff8e75f6c58006d7b0f37db
Abat <kasabat001@gmail.com>
2023-02-21 00:07:12 +0600

    fix: types

--- src/shared/types/api.ts
+++ src/shared/types/api.ts
@@ -16,13 +16,24 @@ interface SuccessResponse<T> {
     message: T;
 }
 
-type ResponseInterface<T> = SuccessResponse<T> | FailResponse;
+type ResponseInterface<T> =
+    | SuccessResponse<T>
+    | FailResponse;
 
-const isSuccessResponse = <T>(response: ResponseInterface<T>): response is SuccessResponse<T> => {
+const isSuccessResponse = <T>(
+    response: ResponseInterface<T>,
+): response is SuccessResponse<T> => {
     return !!(response as SuccessResponse<T>).message;
 };
 
-type FetchInterface<T> = AxiosResponse<ResponseInterface<T>>;
+type FetchInterface<T> = AxiosResponse<
+    ResponseInterface<T>
+>;
 
-export type { FetchInterface, ResponseInterface, SuccessResponse, FailResponse };
+export type {
+    FetchInterface,
+    ResponseInterface,
+    SuccessResponse,
+    FailResponse,
+};
 export { isSuccessResponse, ResponseStatuses };
